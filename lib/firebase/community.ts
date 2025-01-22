20a80b4a8c3a3e73ee6aa99ec96cf037d504ed41
DivorcedLance <divorcedlance@gmail.com>
2025-01-21 20:42:47 -0500

    Implement dynamic retrieval of common questions from Firestore; add function to fetch question by ID

--- lib/firebase/community.ts
+++ lib/firebase/community.ts
@@ -6,11 +6,38 @@ import { db } from "@/lib/firebase/firebaseConfig";
 import { CommonQuestion } from '@/types/commonQuestion';
 import { CommunityPost } from '@/types/communityPost';
 
-import { commonQuestions } from '@/mock/commonQuestions';
 import { getUserDataById } from "./userData";
 
 export async function getCommonQuestions(): Promise<CommonQuestion[]> {
-  return commonQuestions;
+
+  const questions: CommonQuestion[] = [];
+  
+  const questionQuery = query(collection(db, 'CommonQuestion'));
+
+  const questionSnapshot = await getDocs(questionQuery);
+
+  for (const doc of questionSnapshot.docs) {
+    questions.push({
+      id: doc.id,
+      ...doc.data(),
+    } as CommonQuestion);
+  }
+
+  return questions;
+}
+
+export async function getCommonQuestionById(id: string): Promise<CommonQuestion | null> {
+  
+  const questionDoc = await getDoc(doc(db, 'CommonQuestion', id));
+
+  if (!questionDoc.exists()) {
+    return null;
+  }
+
+  return {
+    id: questionDoc.id,
+    ...questionDoc.data(),
+  } as CommonQuestion;
 }
 
 export async function getCommunityPosts(): Promise<CommunityPost[]> {
