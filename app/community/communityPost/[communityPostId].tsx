b37958bf1bddc7dd835d723499ca6bf16cc41e41
KiwiGin <bayeskiwigen@gmail.com>
2025-01-21 22:34:38 -0500

    Refactor community post components to update styling and improve readability; change background colors and text colors for better contrast

--- app/community/communityPost/[communityPostId].tsx
+++ app/community/communityPost/[communityPostId].tsx
@@ -1,5 +1,5 @@
-import { View, Text, FlatList, TextInput, Button, TouchableOpacity } from 'react-native';
-import { useLocalSearchParams , useRouter } from 'expo-router';
+import { View, Text, FlatList, TextInput, Button, TouchableOpacity, Image } from 'react-native';
+import { useLocalSearchParams, useRouter } from 'expo-router';
 import { getCommunityPostById } from '@/lib/firebase/community';
 import { CommunityPost } from '@/types/communityPost';
 import { CommunityPostCard } from '@/components/CommunityPostCard';
@@ -29,14 +29,14 @@ const CommunityPostDetails = () => {
 
   if (!post) {
     return (
-      <View className="flex-1 p-4 bg-gray-100 justify-center items-center">
-        <Text className="text-gray-500">Loading post...</Text>
+      <View className="flex-1 p-4 bg-[#1B1E1A] justify-center items-center">
+        <Text className="text-white">Loading post...</Text>
       </View>
     );
   }
 
   return (
-    <View className="flex-1 p-4 bg-gray-100">
+    <View className="flex-1 p-4 bg-[#1B1E1A]">
       {post.responseTo && (
         <TouchableOpacity
           onPress={() => router.push(`../communityPost/${post.responseTo}`)}
@@ -46,9 +46,22 @@ const CommunityPostDetails = () => {
         </TouchableOpacity>
       )}
 
-      <CommunityPostCard {...post} isDetail={true} isTouchable={false}/>
+      {/* <CommunityPostCard {...post} isDetail={true} isTouchable={false}/> */}
+      <Text className="text-[#D8D8D8] text-sm font-bold mb-4">Preguntas de la Comunidad</Text>
+      <Text className="text-white text-5xl font-bold mb-4">{post.title}</Text>
+      <View className="flex flex-row items-center gap-5">
+        <Image
+          source={{ uri: post.student.profileImgLink }}
+          style={{ width: 30, height: 30, borderRadius: 50 }}
+        />
+        <Text className="text-sm text-white">
+          {post.student.firstName} {post.student.lastName}
+        </Text>
+        <Text className="text-sm text-white">Fecha: {post.date.toLocaleDateString()}</Text>
+      </View>
+      <Text className="text-white text-lg mt-4">{post.content}</Text>
 
-      <Text className="text-lg font-bold mb-2">Answers</Text>
+      <Text className="text-white text-lg font-bold mb-2 mt-10">Respuestas ({post.answers?.length})</Text>
       <FlatList
         data={post.answers}
         keyExtractor={(item) => item.id}
